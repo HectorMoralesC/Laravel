2eaf1beebb3c97ddfdd2fc76b45423f4bab3f66d
HectorMoralesC <hectormoralescortes6@gmail.com>
2023-07-20 16:55:33 +0200

    20-7-23

---
+++ app/Http/Controllers/PaginasController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaginasController extends Controller
+{
+    public function inicio(){
+
+        return view ('welcome');
+
+    }
+    public function serveisweb(){
+
+        return view ('serveisweb');
+
+    }
+    public function fbh(){
+
+        return view ('fbh');
+
+    }
+    public function contacto(){
+
+        return view ('contacto');
+
+    }
+    public function hector(){
+
+        return view ('hector');
+
+    }
+}
